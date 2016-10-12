c70e4136693a474f8a48e9b31d14f2e1b4618054
ninomiyaatsushi <a2c.jahbuilt@gmail.com>
2016-10-12 20:17:17 +0900

    Initial commit

---
+++ core/src/jp/techacademy/atsushi/ninomiya/jumpactiongame/ActivityRequestHandler.java
@@ -0,0 +1,9 @@
+package jp.techacademy.atsushi.ninomiya.jumpactiongame;
+
+/**
+ * Created by ninomiyaatsushi on 2016/10/11.
+ */
+
+public interface ActivityRequestHandler {
+    public void showAds(boolean show);
+}
